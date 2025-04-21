5bc5d1075cf8d8cd74c4892c48db1056082093b8
Lou Mancini <lmancini9@gmail.com>
2025-04-20 20:20:38 -0500

    Day18b

---
+++ day18b/src/utils.rs
@@ -0,0 +1,46 @@
+use crossterm::{cursor, terminal, ExecutableCommand};
+use std::io::{stdout, Write};
+
+#[allow(dead_code)]
+pub fn print_grid(vec: &[Vec<char>]) {
+    let mut stdout = stdout();
+    stdout
+        .execute(terminal::Clear(terminal::ClearType::All))
+        .unwrap();
+
+    stdout.execute(cursor::MoveTo(0, 0)).unwrap();
+
+    for row in vec.iter() {
+        let s: String = row.iter().collect();
+        //println!("{}", s);
+        writeln!(stdout, "{}", s).unwrap();
+    }
+
+    stdout.flush().unwrap();
+}
+
+#[allow(dead_code)]
+pub fn print_grid_opt(vec: &[Vec<Option<usize>>]) {
+    let mut stdout = stdout();
+    stdout
+        .execute(terminal::Clear(terminal::ClearType::All))
+        .unwrap();
+
+    stdout.execute(cursor::MoveTo(0, 0)).unwrap();
+
+    for row in vec.iter() {
+        for entry in row {
+            if let Some(val) = entry {
+                write!(stdout, "{:03} ", val).unwrap();
+            } else {
+                write!(stdout, "### ").unwrap();
+            }
+        }
+
+        writeln!(stdout).unwrap();
+    }
+
+    writeln!(stdout).unwrap();
+
+    stdout.flush().unwrap();
+}
