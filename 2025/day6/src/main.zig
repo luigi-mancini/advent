8a98f3cf80ca1844e8ce19c0becb5d20b1e2198a
Lou Mancini <lmancini9@gmail.com>
2025-12-11 18:25:59 -0600

    temp day6 commit

--- 2025/day6/src/main.zig
+++ 2025/day6/src/main.zig
@@ -4,66 +4,55 @@ const ArrayList = std.ArrayList;
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
 
-const Range = struct { start: u64, end: u64 };
+pub fn parse(comptime T: type, str: []const u8, list: *ArrayList(ArrayList(T))) !void {
+    const init = list.*.items.len == 0;
 
-pub fn doRangesIntersect(r1: Range, r2: Range) bool {
-    if ((r1.start >= r2.start and r1.start <= r2.end) or
-        (r1.end >= r2.start and r1.end <= r2.end) or
-        (r2.start >= r1.start and r2.start <= r1.end) or
-        (r2.end >= r1.start and r2.end <= r1.end))
-    {
-        return true;
-    }
-    return false;
-}
-
-pub fn mergeRanges(r1: Range, r2: Range) Range {
-    return .{ .start = @min(r1.start, r2.start), .end = @max(r1.end, r2.end) };
-}
-
-pub fn addAndMergeRangeToList(r: Range, list: *ArrayList(Range)) !void {
-    var range: Range = r;
+    var it = std.mem.tokenizeAny(u8, str, " ");
 
     var index: usize = 0;
 
-    while (index < list.items.len) {
-        if (doRangesIntersect(range, list.items[index])) {
-            range = mergeRanges(range, list.items[index]);
-            _ = list.swapRemove(index);
-        } else {
-            index += 1;
+    while (it.next()) |val| : (index += 1) {
+        const operand = switch (type) {
+            u64 => try std.fmt.parseInt(T, val, 10),
+            u8 => val,
+            else => val,
+        };
+
+        //
+
+        if (init) {
+            try list.append(allocator, try ArrayList(T).initCapacity(allocator, 10));
         }
+
+        std.debug.assert(index < list.items.len);
+
+        try list.items[index].append(allocator, operand);
     }
-    try list.append(allocator, range);
 }
 
-pub fn countFreshIngredients(ranges: []Range, ids: []u64) u64 {
-    var count: u64 = 0;
-    for (ids) |i| {
-        for (ranges) |r| {
-            if (i >= r.start and i <= r.end) {
-                count += 1;
-                break;
-            }
-        }
-    }
-    return count;
-}
+pub fn parseOperands(str: []u8, list: *ArrayList(ArrayList(u64))) !void {
+    const init = list.*.items.len == 0;
 
-pub fn parseOperands(
-    str: []u8,
-) !void {
     var it = std.mem.tokenizeAny(u8, str, " ");
 
-    while (it.next()) |val| {
+    var index: usize = 0;
+
+    while (it.next()) |val| : (index += 1) {
         const operand = try std.fmt.parseInt(u64, val, 10);
-        try list.append(allocator, operand);
+
+        if (init) {
+            try list.append(allocator, try ArrayList(u64).initCapacity(allocator, 10));
+        }
+
+        std.debug.assert(index < list.items.len);
+
+        list.items[index] = operand;
     }
 }
 
 pub fn readInput(operands: *ArrayList(ArrayList(u64)), operators: *ArrayList(ArrayList(u8))) !void {
     const cwd = std.fs.cwd();
-    const file = try cwd.openFile("day5.txt", .{ .mode = .read_only });
+    const file = try cwd.openFile("test.txt", .{ .mode = .read_only });
     defer file.close();
 
     var read_buffer: [1024]u8 = undefined;
@@ -75,22 +64,9 @@ pub fn readInput(operands: *ArrayList(ArrayList(u64)), operators: *ArrayList(Arr
 
         if (str.len != 0) {
             if (std.ascii.isDigit(str[0])) {
-                parseOperands();
+                try parse(u64, str, operands);
             } else {
-                parseOperators();
-            }
-        }
-
-        if (std.mem.indexOfScalar(u8, str, '-')) |index| {
-            const start = try std.fmt.parseInt(u64, str[0..index], 10);
-            const end = try std.fmt.parseInt(u64, str[index + 1 ..], 10);
-
-            const r: Range = .{ .start = start, .end = end };
-            try addAndMergeRangeToList(r, mergedRange);
-        } else {
-            if (str.len != 0) {
-                const id = try std.fmt.parseInt(u64, str, 10);
-                try ids.append(allocator, id);
+                try parse(u8, str, operators);
             }
         }
         _ = try reader.discardDelimiterInclusive('\n');
@@ -103,7 +79,7 @@ pub fn readInput(operands: *ArrayList(ArrayList(u64)), operators: *ArrayList(Arr
 }
 
 pub fn main() !void {
-    defer _ = gpa.deinit();
+    //defer _ = gpa.deinit();
 
     var operands = try ArrayList(ArrayList(u64)).initCapacity(allocator, 500);
     var operators = try ArrayList(ArrayList(u8)).initCapacity(allocator, 500);
@@ -112,21 +88,16 @@ pub fn main() !void {
 
     try readInput(&operands, &operators);
 
-    //for (ranges.items) |r| {
-    //    std.debug.print("{}\n", .{r});
-    //}
+    for (operands.items) |op| {
+        std.debug.print("LDB {}\n", .{op});
+    }
 
     //for (ids.items) |i| {
     //    std.debug.print("{d}\n", .{i});
     //}
 
-    const count = countFreshIngredients(mergedRanges.items, ids.items);
-    std.debug.print("Part 1: We have {d} fresh ingredients.\n", .{count});
+    // const count = countFreshIngredients(mergedRanges.items, ids.items);
+    //std.debug.print("Part 1: We have {d} fresh ingredients.\n", .{count});
 
-    var mergedCount: u64 = 0;
-    for (mergedRanges.items) |r| {
-        mergedCount += (r.end - r.start) + 1;
-    }
-
-    std.debug.print("Part 2: We have {d} fresh ingredients.\n", .{mergedCount});
+    //std.debug.print("Part 2: We have {d} fresh ingredients.\n", .{mergedCount});
 }
